fbecea5ed2aa0c047eeadf9e3d386350e1218727
bokutotu <mushin.hudoushin@gmail.com>
2024-12-08 17:33:26 +0900

    bkwd conv test

--- tests/conv.cpp
+++ tests/conv.cpp
@@ -68,3 +68,130 @@ TEST_CASE("conv2d", "[conv2d]") {
     status = execute_conv_forward(desc, &buffers, workspace.devPtr, &handle);
     REQUIRE(status == SUCCESS);
 };
+
+TEST_CASE("conv2d backward data", "[conv2d_backwawrd_data]") {
+    cudnnHandle_t handle;
+    cudnnCreate(&handle);
+
+    int64_t n = 16, c = 128, h = 64, w = 64, k = 256, r = 1, s = 1;
+
+    CudnnTensorShapeStride dy_shape = {
+        .num_dims = 4,
+        .dims = {n, k, h, w},
+        .strides = {k * h * w, h * w, w, 1}
+    };
+    CudnnTensorShapeStride filter_shape = {
+        .num_dims = 4,
+        .dims = {k, c, r, s},
+        .strides = {c * r * s, r * s, s, 1}
+    };
+    CudnnTensorShapeStride dx_shape = {
+        .num_dims = 4,
+        .dims = {n, c, h, w},
+        .strides = {c * h * w, h * w, w, 1}
+    };
+    ConvInfo info = {
+        .padding = {0, 0},
+        .stride = {1, 1},
+        .dilation = {1, 1},
+        .num_dims = 2
+    };
+    ConvBkwdDataDescriptor* desc;
+    CudnnFrontendError_t status = create_conv_backward_data_descriptor(&desc, 
+                                                                      DATA_TYPE_FLOAT, 
+                                                                      &dy_shape, 
+                                                                      &filter_shape, 
+                                                                      &dx_shape, 
+                                                                      &info);
+    REQUIRE(status == SUCCESS);
+    CHECK(desc != nullptr);
+
+    status = check_conv_backward_data_graph(desc, &handle);
+    REQUIRE(status == SUCCESS);
+
+    int64_t workspace_size;
+    status = get_conv_backward_data_workspace_size(desc, &workspace_size);
+    REQUIRE(status == SUCCESS);
+
+    ConvBkwdDataBuffers buffers = {
+        .DY = nullptr,
+        .filter = nullptr,
+        .DX = nullptr
+    };
+
+    Surface<float> DY_tensor(n * k * h * w, false);
+    Surface<float> Filter_tensor(k * c * r * s, false);
+    Surface<float> DX_tensor(n * c * h * w, false);
+    Surface<int8_t> workspace(workspace_size, false);
+
+    buffers.DY = DY_tensor.devPtr;
+    buffers.filter = Filter_tensor.devPtr;
+    buffers.DX = DX_tensor.devPtr;
+
+    status = execute_conv_backward_data(desc, &buffers, workspace.devPtr, &handle);
+    REQUIRE(status == SUCCESS);
+};
+
+TEST_CASE("conv2d backward fileter", "[conv2d_backward_fileter]") {
+    cudnnHandle_t handle;
+    cudnnCreate(&handle);
+
+    int64_t n = 16, c = 128, h = 64, w = 64, k = 256, r = 1, s = 1;
+
+    CudnnTensorShapeStride x_shape = {
+        .num_dims = 4,
+        .dims = {n, c, h, w},
+        .strides = {c * h * w, h * w, w, 1}
+    };
+    CudnnTensorShapeStride dy_shape = {
+        .num_dims = 4,
+        .dims = {n, k, h, w},
+        .strides = {k * h * w, h * w, w, 1}
+    };
+    CudnnTensorShapeStride dw_shape = {
+        .num_dims = 4,
+        .dims = {k, c, r, s},
+        .strides = {c * r * s, r * s, s, 1}
+    };
+    ConvInfo info = {
+        .padding = {0, 0},
+        .stride = {1, 1},
+        .dilation = {1, 1},
+        .num_dims = 2
+    };
+    ConvBkwdFilterDescriptor* desc;
+    CudnnFrontendError_t status = create_conv_backward_filter_descriptor(&desc, 
+                                                                        DATA_TYPE_FLOAT, 
+                                                                        &x_shape, 
+                                                                        &dy_shape, 
+                                                                        &dw_shape, 
+                                                                        &info);
+    REQUIRE(status == SUCCESS);
+    CHECK(desc != nullptr);
+
+    status = check_conv_backward_filter_graph(desc, &handle);
+    REQUIRE(status == SUCCESS);
+
+    int64_t workspace_size;
+    status = get_conv_backward_filter_workspace_size(desc, &workspace_size);
+    REQUIRE(status == SUCCESS);
+
+    ConvBkwdFilterBuffers buffers = {
+        .X = nullptr,
+        .DY = nullptr,
+        .DW = nullptr
+    };
+
+    Surface<float> X_tensor(n * c * h * w, false);
+    Surface<float> DY_tensor(n * k * h * w, false);
+    Surface<float> DW_tensor(k * c * r * s, false);
+    Surface<int8_t> workspace(workspace_size, false);
+
+    buffers.X = X_tensor.devPtr;
+    buffers.DY = DY_tensor.devPtr;
+    buffers.DW = DW_tensor.devPtr;
+
+    status = execute_conv_backward_filter(desc, &buffers, workspace.devPtr, &handle);
+    REQUIRE(status == SUCCESS);
+    cudnnDestroy(handle);
+};
